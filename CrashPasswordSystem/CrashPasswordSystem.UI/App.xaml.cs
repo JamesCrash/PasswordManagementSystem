6502f9a9784a3e81b16c6c27317004156f2724ea
Esteban Solano Granados <stvansolano@outlook.com>
2019-08-09 09:09:07 -0600

    Documented: App composition (Shell, Bootstrapper and Module Catalog)

--- CrashPasswordSystem/CrashPasswordSystem.UI/App.xaml.cs
+++ CrashPasswordSystem/CrashPasswordSystem.UI/App.xaml.cs
@@ -8,6 +8,11 @@ using System.Windows.Threading;
 
 namespace CrashPasswordSystem.UI
 {
+    /// <summary>
+    /// Represents the Main Application container (Prism app).
+    /// 
+    /// Its primary usage includes the Bootstrapper and Shell (Main) Window.
+    /// </summary>
     public partial class App : PrismApplication
     {
         public Bootstrapper Bootstrapper { get; private set; }
@@ -27,6 +32,25 @@ namespace CrashPasswordSystem.UI
             Bootstrapper.RegisterTypes(containerRegistry);
         }
 
+
+        /// <summary>
+        /// Enables the module catalog initialization so all modules marked as [Module] can be discovered.
+        /// 
+        /// Module considerations
+        /// - Not all the modules in the app are placed in regions. Just a few of them. Then you can have modules 
+        /// interacting with other sub-modules or views through EventAggregator.
+        /// 
+        /// - Modules are intented to be individual and reduce dependencies on them to make them re-usable.
+        ///
+        /// - Modules can be wired up with MVVM by re-using code. In order to follow MVVM auto-wire model from Prism,
+        /// every module may need to define its 'DataContext' to be its ViewModel class.
+        /// 
+        /// - The ViewModel hierarchy starts from 'MainViewModel' since 'MainWindow' is the root UI. All subsequent views
+        /// come into the 'MainViewModel' hierarchy and its ViewModel descentdants (e.g 'Home' or )
+        ///  
+        /// Follow-up other (basic) modules in the app including 'ApplicationBar' and 'Login' to follow-up in the code
+        /// </summary>
+        /// <param name="catalog"></param>
         protected override void ConfigureModuleCatalog(IModuleCatalog catalog)
         {
             Bootstrapper.ConfigureModuleCatalog(catalog);
